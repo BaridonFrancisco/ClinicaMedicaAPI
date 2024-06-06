7ea46661344918b78a40264dbcb2f6b999a8e43c
BaridonFrancisco <baridonfrancisco@gmail.com>
2024-06-06 19:56:11 -0300

    Spring Security agregando y clase de logeo

--- src/main/java/com/clinicaMed/clinicaMedica/controller/MedicoController.java
+++ src/main/java/com/clinicaMed/clinicaMedica/controller/MedicoController.java
@@ -17,7 +17,7 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.util.UriComponentsBuilder;
 
 import java.net.URI;
-import java.util.List;
+
 
 @RestController
 @RequestMapping("/medicos")
---
+++ src/main/java/com/clinicaMed/clinicaMedica/model/Usuario.java
@@ -0,0 +1,19 @@
+package com.clinicaMed.clinicaMedica.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity(name = "usuario")
+@Table(name = "usuarios")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode (of = "id")
+public class Usuario {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String login;
+    private String clave;
+}
